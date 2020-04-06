7b734827b190790997ca978b1da7a78acdbd2b53
Mathullah <54320552+Mathullah@users.noreply.github.com>
2020-04-06 13:09:26 +0200

    Removed unused pin

--- SoundCheck/SoundCheck.ino
+++ SoundCheck/SoundCheck.ino
@@ -1,4 +1,3 @@
-const uint16_t MIN_TONE_FREQUENCY(100);
 const uint16_t MIN_TONE_FREQUENCY(440);
 const uint16_t MAX_TONE_FREQUENCY(5000);
 const uint16_t FREQUENCY_STEP(40);
@@ -26,7 +25,4 @@ void loop() {
       tone(OUTPUT_PIN, Frequency);
       delay(20);
   }
-
-  noTone(OUTPUT_PIN);
-  
 }
