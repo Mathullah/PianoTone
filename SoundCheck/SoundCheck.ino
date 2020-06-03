5ab7d6eda181fac70cd731eeb888978610815c4b
Mathullah <54320552+Mathullah@users.noreply.github.com>
2020-06-03 12:13:29 +0200

    Update SoundCheck.ino
    Code reworked for testing of float numbers

--- SoundCheck/SoundCheck.ino
+++ SoundCheck/SoundCheck.ino
@@ -4,13 +4,17 @@ const uint16_t FREQUENCY_STEP(40);
 
 const uint8_t OUTPUT_PIN(8);
 
+const float tone_C6(1046.5F);
+const float tone_E6(1318.51F);
+const float tone_G6(1567.98F);
+
 void setup() {
 
-  tone(OUTPUT_PIN, 1047);
+  tone(OUTPUT_PIN, static_cast<uint16_t>(tone_C6));
   delay(500);
-  tone(OUTPUT_PIN, 1319);
+  tone(OUTPUT_PIN, static_cast<uint16_t>(tone_E6));
   delay(500);
-  tone(OUTPUT_PIN, 1568);
+  tone(OUTPUT_PIN, static_cast<uint16_t>(tone_G6));
   delay(500);
   noTone(OUTPUT_PIN);  
   delay(500);
@@ -18,9 +22,7 @@ void setup() {
 
 void loop() {
 
-  uint16_t Frequency (MIN_TONE_FREQUENCY);
-
-  for (Frequency; Frequency < MAX_TONE_FREQUENCY; Frequency += FREQUENCY_STEP)
+  for (uint16_t Frequency = MIN_TONE_FREQUENCY; Frequency < MAX_TONE_FREQUENCY; Frequency += FREQUENCY_STEP)
   {
       tone(OUTPUT_PIN, Frequency);
       delay(20);
