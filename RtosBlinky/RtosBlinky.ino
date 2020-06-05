3cb41eb6fd27d53e8842a324e2bc69f2beec7ff1
Mathullah <54320552+Mathullah@users.noreply.github.com>
2020-06-05 18:40:35 +0200

    Update RtosBlinky.ino
    Simple debouncing function added into interrupt call. Made some code cleanup

--- RtosBlinky/RtosBlinky.ino
+++ RtosBlinky/RtosBlinky.ino
@@ -7,13 +7,20 @@ void TaskBeep(void *pvParameters);
 
 static const uint16_t main_1s_dealy = 1000 / portTICK_PERIOD_MS;
 static const uint16_t main_0s5_dealy = 500 / portTICK_PERIOD_MS;
-static const uint16_t main_10ms_dealy = 20 / portTICK_PERIOD_MS;
+static const uint16_t main_0s1_dealy = 100 / portTICK_PERIOD_MS;
 
-static const uint16_t Periode = (34286);
+static const uint16_t Periode(65535 - 100);
+
+
+static const unsigned PUSH_BUTTON(6); 
+
+volatile uint16_t State(0);
+volatile bool DebouncedInput(false);
 
 void setup() {
 
-    pinMode(LED_BUILTIN, OUTPUT);  // Ausgabe LED festlegen
+    pinMode(PUSH_BUTTON, INPUT_PULLUP);
+    pinMode(LED_BUILTIN, OUTPUT);
   
     // Timer 1
     noInterrupts();           // Alle Interrupts temporär abschalten
@@ -21,11 +28,14 @@ void setup() {
     TCCR1B = 0;
   
     TCNT1 = Periode;            // Timer nach obiger Rechnung vorbelegen
-    TCCR1B |= (1 << CS12);    // 256 als Prescale-Wert spezifizieren
-    TIMSK1 |= (1 << TOIE1);   // Timer Overflow Interrupt aktivieren
-    interrupts();             // alle Interrupts scharf schalten
+    TCCR1B |= (1 << CS12);      // 256 als Prescale-Wert spezifizieren
+    TIMSK1 |= (1 << TOIE1);     // Timer Overflow Interrupt aktivieren
+    interrupts();
 
-    //xTaskCreate(TaskBlink, "Blink", 128, NULL, 2, NULL );
+
+    xTaskCreate(TaskMonitor, "Monitor", 128, NULL, 2, NULL );
+    xTaskCreate(TaskBeep,    "Beep",    128, NULL, 2, NULL );
+    xTaskCreate(TaskBlink,   "Blink",   128, NULL, 2, NULL );
 
 }
 
@@ -38,50 +48,52 @@ void TaskBlink(void *pvParameters)
 {
   (void) pvParameters;
 
-  //pinMode(LED_BUILTIN, OUTPUT);
   TickType_t LastWakeTime = xTaskGetTickCount();
-
-  xTaskCreate(TaskMonitor, "Monitor", 128, NULL, 2, NULL );
-  xTaskCreate(TaskBeep,    "Beep",    128, NULL, 2, NULL );
 
   for (;;)
   {
-      vTaskDelayUntil(&LastWakeTime, main_0s5_dealy);
+      vTaskDelayUntil(&LastWakeTime, main_0s1_dealy);
     
-      //unsigned NewState = (digitalRead(LED_BUILTIN) ? LOW : HIGH);
-      //digitalWrite(LED_BUILTIN, NewState);
+      unsigned NewState = (DebouncedInput ? HIGH : LOW);
+      digitalWrite(LED_BUILTIN, NewState);
   }
 }
 
 void TaskMonitor(void *pvParameters)
 {
-  (void) pvParameters;
-
-  TickType_t LastWakeTime = xTaskGetTickCount();
-
-  for (;;)
-  {
-      vTaskDelayUntil(&LastWakeTime, main_1s_dealy);
-
-      //unsigned NewState = (digitalRead(LED_BUILTIN) ? LOW : HIGH);
-      //digitalWrite(LED_BUILTIN, NewState);
-  }
+    (void) pvParameters;
+    
+    TickType_t LastWakeTime = xTaskGetTickCount();
+    
+    for (;;)
+    {
+        vTaskDelayUntil(&LastWakeTime, main_1s_dealy);
+    
+    }
 }
 
 void TaskBeep(void *pvParameters)
 {
-  (void) pvParameters;
-
-  for (;;)
-  {
-
-  }
+    (void) pvParameters;
+    
+    for (;;)
+    {
+    
+    }
 }
 
-// Hier kommt die selbstdefinierte Interruptbehandlungsroutine 
-// für den Timer Overflow
+/**
+ * Timer 1 Overflow ISR
+ * https://www.embedded.com/my-favorite-software-debouncers/
+ */
 ISR(TIMER1_OVF_vect)        
 {
-  TCNT1 = Periode;
-  digitalWrite(LED_BUILTIN, digitalRead(LED_BUILTIN) ^ 1);
+    TCNT1 = Periode;
+    
+    uint16_t input = digitalRead(PUSH_BUTTON) ? 0 : 1;
+    
+    State = (State << 1) | input;
+
+    if(State == 0x0000) { DebouncedInput = false; }
+    if(State == 0xFFFF) { DebouncedInput = true; }
 }
